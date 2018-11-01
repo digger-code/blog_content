b36ff22dc53f6aaf322e3a642ae5b60a9d66cce3
prozhou <605840905@qq.com>
2018-11-01 22:02:18 +0800

    config file

--- init.sh
+++ init.sh
@@ -12,6 +12,7 @@ source_dir=`pwd`
 [ ! -d $blog_dir ] && mkdir $blog_dir && cd $blog_dir && hexo init && cd $source_dir 
 
 #make hexo use current source dir
+cp "$source_dir/etc/_config.yml"  "$blog_dir/_config.yml" && \
 rm -rf $blog_dir/source && ln -s $source_dir/source $blog_dir/source && \
 cd "../blog" && npm install hexo-deployer-git --save 
 
